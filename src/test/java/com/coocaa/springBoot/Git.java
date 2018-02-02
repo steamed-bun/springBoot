0e56ab421b3ee111a2cf91d743631a12727e3f69
steamed-bun <w514156@hotmail.com>
2018-02-02 10:06:57 +0800

    dev 修改

--- src/test/java/com/coocaa/springBoot/Git.java
+++ src/test/java/com/coocaa/springBoot/Git.java
@@ -1,4 +1,10 @@
 package com.coocaa.springBoot;
 
 public class Git {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println("test1");
+    }
+
 }
